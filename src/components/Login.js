d0164b52d63e8725247703690ca431c9d48126c2
Amogh Warkhandkar <amoghw2025@gmail.com>
2019-08-31 21:52:37 +0530

    Login: Complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,64 @@
-import React from "react";
-import { Typography } from "@material-ui/core";
+import React, { useState, useContext, useEffect } from "react";
+import { withRouter, Redirect } from "react-router-dom";
+import { Typography, TextField, Button } from "@material-ui/core";
+import fire_app from "../base";
+import { AuthContext } from "../auth";
 
-export default function Login() {
-  return <Typography>Login</Typography>;
-}
+const Login = ({ history }) => {
+
+
+  const handleLogin = () => {
+    try {
+      const { email, password } = values;
+      fire_app.auth().signInWithEmailAndPassword(email, password);
+      history.push("/");
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const [values, setValues] = useState({ email: "", password: "" });
+
+  const { currentUser } = useContext(AuthContext);
+  if (currentUser) {
+    return <Redirect to="/"></Redirect>;
+  }
+
+  return (
+    <React.Fragment>
+      <Typography>Login</Typography>
+      <TextField
+        name="email"
+        label="Email"
+        margin="normal"
+        value={values.email}
+        onChange={handleInputChange}
+      ></TextField>
+      <br></br>
+      <TextField
+        name="password"
+        label="Password"
+        type="password"
+        margin="normal"
+        value={values.password}
+        onChange={handleInputChange}
+      ></TextField>
+      <br></br>
+      <Button
+        variant="contained"
+        color="secondary"
+        style={{ marginTop: "16px", marginBottom: "8px" }}
+        onClick={() => handleLogin()}
+      >
+        Login
+      </Button>
+    </React.Fragment>
+  );
+};
+
+export default withRouter(Login);
