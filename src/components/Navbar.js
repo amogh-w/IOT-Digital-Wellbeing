82676302998bd93841f819dc7587e1af7628d337
Amogh Warkhandkar <amoghw2025@gmail.com>
2019-09-01 12:26:58 +0530

    Navbar: Added Conditional Rendering

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AppBar, Toolbar, Typography, Tabs, Tab } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../auth";
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography style={{ flex: 1}}>Digital Wellbeing</Typography>
-          <Tabs>
+        <Typography style={{ flex: 1 }}>Digital Wellbeing</Typography>
+        <Tabs>
+          {!currentUser ? (
+            <React.Fragment>
+              <Tab label="Login" component={Link} to="/login"></Tab>
+              <Tab label="SignUp" component={Link} to="/signup"></Tab>
+            </React.Fragment>
+          ) : (
             <Tab label="Dashboard" component={Link} to="/"></Tab>
-            <Tab label="Login" component={Link} to="/login"></Tab>
-            <Tab label="SignUp" component={Link} to="/signup"></Tab>
-          </Tabs>
+          )}
+        </Tabs>
       </Toolbar>
     </AppBar>
   );
